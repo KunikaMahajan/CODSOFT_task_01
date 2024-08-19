00ba45aa47097e86e51887ab7a9caf87d04746f1
KunikaMahajan <143798830+KunikaMahajan@users.noreply.github.com>
2024-08-19 18:40:55 +0530

    Add files via upload

---
+++ task01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+    int userGuess = 0;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have selected a number between 1 and 100." << endl;
+
+    // Loop until the user guesses the correct number
+    do {
+        cout << "Enter your guess: ";
+        cin >> userGuess;
+        attempts++;
+
+        if (userGuess < randomNumber) {
+            cout << "Too low! Try again." << endl;
+        } else if (userGuess > randomNumber) {
+            cout << "Too high! Try again." << endl;
+        } else {
+            cout << "Congratulations! You've guessed the correct number: " << randomNumber << endl;
+            cout << "It took you " << attempts << " attempts." << endl;
+        }
+    } while (userGuess != randomNumber);
+
+    return 0;
+}
